7107597d2d5f371889ce69912b92cf642211dde2
RustambekSafarov <rustambeksafarovv@gmail.com>
2022-11-23 16:11:48 +0500

    done

--- find_max_key.py
+++ find_max_key.py
@@ -6,4 +6,11 @@ def find_max_key(data: dict) -> int:
     Returns:
         int: The maximum key in the dictionary.
     """
-    pass
+    s = list(data.keys())[0]
+    for i in data.keys():
+        if int(i) > s:
+            s = i
+    return s
+
+
+print(find_max_key({1: 'a', 2: 'b', 3: 'c'}))
