deb6c2b8d34b4b92baec468df8b5dab15124ec27
RustambekSafarov <rustambeksafarovv@gmail.com>
2022-11-23 16:13:29 +0500

    done

--- find_max_value.py
+++ find_max_value.py
@@ -6,4 +6,11 @@ def find_max_value(data: dict) -> int:
     Returns:
         int: The maximum value in the dictionary.
     """
-    pass
+    m = list(data.values())[0]
+    for i in data.values():
+        if m < i:
+            m = i
+    return m
+
+
+print(find_max_value({'a': 1, 'b': 2, "c": 3}))
